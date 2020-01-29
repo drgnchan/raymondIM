6a621bea132401989425e91a94c8729100147442
drgnchan <raymond2366@outlook.com>
2020-01-29 22:23:36 +0800

    first commit

---
+++ src/main/java/com/raymond/comct/ComctApplication.java
@@ -0,0 +1,13 @@
+package com.raymond.comct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raymond/comct/ComctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymond.comct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
